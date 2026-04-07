10d1ca43e4bb59df821a1c96633956a0de7ca1c1
Teddy <teddyhozaesun@gmail.com>
2026-04-07 20:01:42 +1200

    Add files via upload

---
+++ StudentGradeTracker/StudentGradeTracker/StudentGradeTracker.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace StudentGradeTracker
+{
+    partial class StudentGradeTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxStudent = new System.Windows.Forms.TextBox();
+            this.textBoxGrade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxStudent
+            // 
+            this.textBoxStudent.Location = new System.Drawing.Point(314, 33);
+            this.textBoxStudent.Name = "textBoxStudent";
+            this.textBoxStudent.Size = new System.Drawing.Size(128, 20);
+            this.textBoxStudent.TabIndex = 0;
+            // 
+            // textBoxGrade
+            // 
+            this.textBoxGrade.Location = new System.Drawing.Point(314, 59);
+            this.textBoxGrade.Name = "textBoxGrade";
+            this.textBoxGrade.Size = new System.Drawing.Size(128, 20);
+            this.textBoxGrade.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grade:";
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.ItemHeight = 14;
+            this.listBoxOutput.Location = new System.Drawing.Point(12, 12);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(238, 424);
+            this.listBoxOutput.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(272, 130);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(170, 58);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "&Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Location = new System.Drawing.Point(272, 194);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(170, 58);
+            this.buttonDisplay.TabIndex = 6;
+            this.buttonDisplay.Text = "&Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(272, 256);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(170, 58);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "&Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 473);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxGrade);
+            this.Controls.Add(this.textBoxStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxStudent;
+        private System.Windows.Forms.TextBox textBoxGrade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDisplay;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ StudentGradeTracker/StudentGradeTracker/StudentGradeTracker.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeTracker
+{
+    /// <summary>
+    /// This program will take the students name and grade then add it to a list. Afterwards it will display the students name seperately with their initial 
+    /// grade/ if they passed or failed, sorted from highest grade to lowest grade. It will also display the average grade, highest grade, and lowest grade 
+    /// of the students in the list. 
+    /// Author: Teddy Sun
+    /// Last Modified: 7/04/2026
+    /// </summary>
+    public partial class StudentGradeTracker : Form
+    {
+        //Declare Collections
+        new List<string> studentNames = new List<string>();
+        new List<double> studentGrades = new List<double>();
+        //Declare constants
+        const int MAX_GRADE = 100;
+        const int MIN_GRADE = 0;
+        const int PASSING_GRADE = 50;
+        public StudentGradeTracker()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Simple method to display the items into the listbox easier and cleaner
+        /// </summary>
+        /// <param name="display"></param>
+        private void display(string display)
+        {
+            listBoxOutput.Items.Add(display);
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //Checks if the user input is valid, including if the grade is a number between MIN and MAX and if the student name is not empty
+            if (double.TryParse(textBoxGrade.Text, out double grade) && grade >= MIN_GRADE && grade <= MAX_GRADE && textBoxStudent.Text != "")
+            {
+                //adds the students grade and names to their lists
+                studentGrades.Add(grade);
+                studentNames.Add(textBoxStudent.Text);
+                //Clears the textboxes and focuses the cursor on the student name textbox
+                textBoxGrade.Clear();
+                textBoxStudent.Clear();
+                textBoxStudent.Focus();
+            }
+            else
+            {
+                //Display relevant error message if the user input is invalid
+                MessageBox.Show($"Please enter a valid student name and grade ({MIN_GRADE}-{MAX_GRADE}).");
+            } 
+        }
+        /// <summary>
+        /// Displays the acutal results into a listbox, including the students name,
+        /// grade, average grade, highest grade, and lowest grade. It also shows the 
+        /// relevant error message if there are no students in the list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            //checks if there is atleast one item within the list
+            if (studentGrades.Count != 0)
+            {
+                //displays the studen with their grade
+                display("--------STUDENT RESULTS--------");
+                for (int i = 0; i < studentGrades.Count; i++)
+                {
+                    //checks if the students grade is above or equal to the passing grade
+                    if (studentGrades[i] >= PASSING_GRADE)
+                    {
+                        //displays the students name, grade, and passed status
+                        display($"{studentNames[i]}: {studentGrades[i]} PASSED");
+                    }
+                    else
+                    {
+                        //displays the students name, grade, and failed status
+                        display($"{studentNames[i]}: {studentGrades[i]} FAILED");
+                    }
+                }
+                //displays the average grade, highest grade, and lowest grade of the students in the list
+                display("-------------------------------");
+                display($"AVERAGE: {Math.Round(studentGrades.Average(), 2)}");
+                display($"HIGHEST: {studentGrades.Max()}");
+                display($"LOWEST: {studentGrades.Min()}");
+            }
+            else
+            {
+                //Shows the relevant error message if there are no students in the list
+                MessageBox.Show("No students in the list. Please add students before displaying."); 
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            //Clears the collections, listbox, textboxes, and focuses the cursor on the student name textbox
+            listBoxOutput.Items.Clear();
+            textBoxGrade.Clear();
+            textBoxStudent.Clear();
+            textBoxStudent.Focus();
+            studentGrades.Clear();
+            studentNames.Clear();
+        }
+    }
+}
